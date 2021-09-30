59783f2a137661297aec84553e74f4ae0ba32c24
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-09-30 15:35:38 -0300

    Error to create movie.mvc

---
+++ Movies.MVC/Data/MoviesMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.MVC.Models;
+
+namespace Movies.MVC.Data
+{
+    public class MoviesMVCContext : DbContext
+    {
+        public MoviesMVCContext (DbContextOptions<MoviesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.MVC.Models.Movie> Movie { get; set; }
+    }
+}
