ca59923889cc63a864429773ee3dac7345805967
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-10-04 15:59:22 -0300

    Bulding ocelot api gateway microservice

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApiGateway
 {
@@ -18,7 +13,7 @@ namespace ApiGateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) => 
+                .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.AddJsonFile("ocelot.json");
                 })
