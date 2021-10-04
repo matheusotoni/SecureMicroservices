0973236d989407599c0bf98e705b5fb0b745f1e5
Matheus Otoni Amorim Silva <matheus.amorim@havan.com.br>
2021-10-04 16:07:52 -0300

    Security ocelot api gateway with bearer token

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -13,6 +14,20 @@ namespace ApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var authenticationProviderKey = "IdentityApiKey";
+
+            // NUGET - Microsoft.AspNetCore.Authentication.JwtBearer
+            services.AddAuthentication()
+             .AddJwtBearer(authenticationProviderKey, x =>
+             {
+                 x.Authority = "https://localhost:5005"; // IDENTITY SERVER URL
+                 //x.RequireHttpsMetadata = false;
+                 x.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateAudience = false
+                 };
+             });
+
             services.AddOcelot();
         }
 
